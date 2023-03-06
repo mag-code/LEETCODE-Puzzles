4e00250f2221845bef6989b06aa4a32e30707d73
MaggotSuffix <119399366+maggotsuffix@users.noreply.github.com>
2023-03-05 23:53:08 -0800

    adding valid anagram
    two solutions

---
+++ python/242. Valid Anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+#solution 1 
+        if len(s) == len(t):
+            d1 = {i:0 for i in s}
+            d2 = {i:0 for i in t}
+            for i in s:
+                d1[i]+=1
+            for i in t:
+                d2[i]+=1
+            if d1==d2:
+                return True
+            return False
+        return False
+#solution 2
+        if len(s) == len(t):           
+            news,newt=sorted(s),sorted(t)
+            return True if news == newt else False
